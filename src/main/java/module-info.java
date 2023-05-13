ec9ac4d2b9cc7d9ecfd5390eb6646ee4906ba2e8
Ralf Spöth <ralf.spoeth@profidata.com>
2023-05-13 16:57:53 +0200

    make initial version work

---
+++ src/main/java/module-info.java
@@ -0,0 +1,4 @@
+module com.github.ralfspoeth.jdbccp {
+    requires java.sql;
+    uses java.sql.Driver;
+}
---
+++ src/test/java/com/github/ralfspoeth/jdbccp/PoolTest.java
@@ -0,0 +1,11 @@
+package com.github.ralfspoeth.jdbccp;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class PoolTest {
+    @Test
+    void testPool() {
+        Assertions.fail("todo");
+    }
+}
